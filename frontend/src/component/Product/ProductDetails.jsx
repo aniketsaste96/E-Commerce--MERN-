182d653eb93fcd78abef3ba9ef8243dd1132b134
Aniket <aniketsaste96@gmail.com>
2022-04-15 03:02:11 +0530

    fetch p deatails

--- frontend/src/component/Product/ProductDetails.jsx
+++ frontend/src/component/Product/ProductDetails.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 import "./ProductDetails.css";
+import Carousel from "react-material-ui-carousel";
+import { useSelector, useDispatch } from "react-redux";
+import { getProductDetails } from "../../actions/productAction";
 const ProductDetails = () => {
-  return <div>ProductDetails</div>;
+  return (
+    <>
+      <div className="ProductDetails">
+        <div>
+          <Carousel>
+            {product.images &&
+              product.images.map((item, i) => (
+                <img
+                  src={item.url}
+                  className="CarouselImage"
+                  key={item.url}
+                  alt={`${i} Slide`}
+                />
+              ))}
+          </Carousel>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ProductDetails;
