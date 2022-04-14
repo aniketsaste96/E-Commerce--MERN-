341a11830373fec665c481c89eb43fd80dc1487f
Aniket <aniketsaste96@gmail.com>
2022-04-14 17:19:39 +0530

    home

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from "./component/layout/Header/Header"
+import { ReactNavbar } from "overlay-navbar"
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import WebFont from "webfontloader";
+import React, { useEffect } from "react";
+import Footer from './component/layout/Footer/Footer';
+import Home from './component/Home/Home';
+
 
 function App() {
+  React.useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Roboto", "Droid Sans", "Chilanka"]
+      }
+    })
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router >
+      <Header />
+      <Route excat path="/">
+        <Home />
+      </Route>
+      <Footer />
+    </Router>
   );
 }
 
