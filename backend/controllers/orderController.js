d205991051c0d6a611c5d1a5fd9185d9359ecf6e
Aniket <aniketsaste96@gmail.com>
2022-04-14 13:32:49 +0530

    fronteEnd

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,6 +1,6 @@
 const Order = require("../models/orderModel");
 const Product = require("../models/productModel");
-const ErrorHander = require("../utils/errorhander");
+const ErrorHandler = require("../utils/errorhandler");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 
 // Create new Order
@@ -52,7 +52,7 @@ exports.getSingleOrder = catchAsyncErrors(async (req, res, next) => {
     );
 
     if (!order) {
-        return next(new ErrorHander("Order not found with this Id", 404));
+        return next(new ErrorHandler("Order not found with this Id", 404));
     }
 
     res.status(200).json({
@@ -116,11 +116,11 @@ exports.updateOrder = catchAsyncErrors(async (req, res, next) => {
     const order = await Order.findById(req.params.id);
 
     if (!order) {
-        return next(new ErrorHander("Order not found with this Id", 404));
+        return next(new ErrorHandler("Order not found with this Id", 404));
     }
 
     if (order.orderStatus === "Delivered") {
-        return next(new ErrorHander("You have already delivered this order", 400));
+        return next(new ErrorHandler("You have already delivered this order", 400));
     }
 
     if (req.body.status === "Shipped") {
@@ -150,7 +150,7 @@ exports.updateOrder = catchAsyncErrors(async (req, res, next) => {
 
 async function updateStock(id, quantity) {
     const product = await Product.findById(id);
-
+    //S capital
     product.Stock -= quantity;
 
     await product.save({ validateBeforeSave: false });
@@ -177,7 +177,7 @@ exports.deleteOrder = catchAsyncErrors(async (req, res, next) => {
     const order = await Order.findById(req.params.id);
 
     if (!order) {
-        return next(new ErrorHander("Order not found with this Id", 404));
+        return next(new ErrorHandler("Order not found with this Id", 404));
     }
 
     await order.remove();
