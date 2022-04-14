4dd68f038c8cf6805c218ef8bd38a0efbc8f0799
Aniket <aniketsaste96@gmail.com>
2022-04-14 18:57:24 +0530

    products FE

--- frontend/src/component/Home/Home.jsx
+++ frontend/src/component/Home/Home.jsx
@@ -1,7 +1,40 @@
 import React from "react";
 import "./home.css";
+import { CgMouse } from "react-icons/cg";
+import Product from "./Product";
+
+const product = {
+  name: "Tshirt",
+  price: "3000",
+  _id: "aniket",
+  images: [{ url: "https://i.ibb.co/DRST11n/1.webp" }],
+};
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <div className="banner">
+        <p>Welcome to e-Commerce</p>
+        <h1>FIND AMAZING PRODUCTS BELOW</h1>
+        <a href="#container">
+          <button>
+            Scroll <CgMouse />
+          </button>
+        </a>
+      </div>
+      <h2 className="homeHeading">Featured Products</h2>
+      <div className="container" id="container">
+        <Product product={product} />
+        <Product product={product} />
+        <Product product={product} />
+        <Product product={product} />
+        <Product product={product} />
+        <Product product={product} />
+        <Product product={product} />
+        <Product product={product} />
+      </div>
+    </>
+  );
 };
 
 export default Home;
